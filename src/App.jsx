de7b0666283ddc48c748b79987bc9c0cae45a552
Ruwy123 <123378428+Ruwy123@users.noreply.github.com>
2025-12-19 01:06:33 +0000

    3d box model

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import ProductViewer from "./components/Productviewer";
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   );
 };
---
+++ src/components/Productviewer.jsx
@@ -0,0 +1,76 @@
+import clsx from "clsx";
+import useMacbookStore from "../store/index.js";
+import { Canvas } from "@react-three/fiber";
+import { Box } from "@react-three/drei";
+
+const ProductViewer = () => {
+  const { color, scale, setColor, setScale } = useMacbookStore();
+  return (
+    <section className=" container relative min-h-[93vh] mx-auto px-5 2xl:px-0 mt-40">
+      <h2 className="text-white font-semibold text-3xl lg:text-6xl">
+        Take a closer look
+      </h2>
+      {/* the colors selectores */}
+      <div className=" absolute z-50 bottom-40 lg:bottom-40 2xl:bottom-52 left-1/2 -translate-x-1/2">
+        <p className="text-xs text-white bg-black/20 px-2 py-1 rounded-lg">
+          MacbookPro {scale} in {color}
+        </p>
+        <div className="flex items-center justify-center gap-5 mt-5">
+          <div className="flex-between bg-neutral-800 rounded-full px-6 py-3 gap-4">
+            <div
+              onClick={() => setColor("#adb5bd")}
+              className={clsx(
+                "size-7 rounded-full cursor-pointer bg-neutral-300",
+                color === "#adb5bd" && "ring-5 size-5 ring-light-100"
+              )}
+            />
+            <div
+              onClick={() => setColor("#2e2c2e")}
+              className={clsx(
+                "size-7 rounded-full cursor-pointer bg-neutral-900",
+                color === "#2e2c2e" && "ring-5 size-5 ring-light-100"
+              )}
+            />
+          </div>
+          {/* the size selectors */}
+          <div className="small-medium flex-between bg-neutral-800 rounded-full px-6 py-2.5 gap-2">
+            <div
+              onClick={() => setScale("0.06")}
+              className={clsx(
+                "size-8 rounded-full cursor-pointer flex-center",
+                scale === "0.06"
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
+              <p>14"</p>
+            </div>
+            <div
+              onClick={() => setScale("0.08")}
+              className={clsx(
+                "size-8 rounded-full cursor-pointer  flex-center",
+                scale === "0.08"
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
+              <p>16"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* the 3d model viewer */}
+      <Canvas
+        className="w-full! h-[80vh]! lg:h-dvh! relative z-40"
+        camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
+      >
+        <Box
+          position={[0, 0, 0]}
+          scale={10 * scale}
+          material-color={color}
+        ></Box>
+      </Canvas>
+    </section>
+  );
+};
+export default ProductViewer;
