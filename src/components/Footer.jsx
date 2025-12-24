96263f33e1a4b9ba5548672b8b16a8e146a4225f
Ruwy123 <123378428+Ruwy123@users.noreply.github.com>
2025-12-24 01:59:05 +0000

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,30 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
-  return <div id="footer">Footer</div>;
+  return (
+    <footer className="container mx-auto py-7 max-lg:px-5">
+      <div className="flex-between">
+        <p className=" text-sm font-medium">
+          More ways to shop: Find an Apple Store or other retailer near you. Or
+          call 033523434634
+        </p>
+        <img src="/logo.svg" alt="logo" />
+      </div>
+      <hr className="my-7 text-[#424245]" />
+      <div className="flex flex-col lg:flex-row lg:items-center justify-between max-lg:mt-5 gap-5 text-sm;">
+        <p>Copyright © 2023 Apple Inc. All rights reserved.</p>
+        <ul className="lg:divide-x flex flex-col lg:flex-row gap-2.5">
+          {footerLinks.map(({ label, link }) => (
+            <li
+              className="cursor-pointer hover:text-blue-500 transition-all duration-300 ease-in-out"
+              key={label}
+            >
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
